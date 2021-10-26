f811bae45f7ba4790133e0d6e1758f2185bb9674
wilburzjh <wilburzjh@gmail.com>
2021-10-26 17:33:55 -0400

    Link like to other APIs

--- django_backend/tweets/api/serializer.py
+++ django_backend/tweets/api/serializer.py
@@ -2,13 +2,33 @@ from rest_framework import serializers
 from accounts.api.serializer import UserSerializerForTweet
 from tweets.models import Tweet
 from comments.api.serializers import CommentSerializer
+from likes.services import LikeService
+
 
 class TweetSerializer(serializers.ModelSerializer):
     user = UserSerializerForTweet() # used to deep check the user from tweet model. call serializer inside another serializer.
+    has_liked = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Tweet
         fields = ('id', 'user', 'created_at', 'content')
+
+    # 自定义的like_set。
+    def get_likes_count(self, obj):
+        return obj.like_set.count()
+
+    # Django提供的comment_set。
+    def get_comments_count(self, obj):
+        return obj.comment_set.count();
+
+    def get_has_liked(self, obj):
+        return LikeService.has_liked(
+            user=self.context['request'].user,
+            target=obj,
+        )
 
 class TweetSerializerForCreate(serializers.ModelSerializer):
     content = serializers.CharField(min_length=6, max_length=140)
